6f702d97a278efb3c84392ed64919c2df5dbb3e1
Jude kuti <judekuti@gmail.com>
2020-07-04 03:24:51 -0500

    Handle CORS issues during testing and dev

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
