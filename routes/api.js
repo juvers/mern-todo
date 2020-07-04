1165e2d78088053562d44fbd91f323d11e617f29
Jude kuti <judekuti@gmail.com>
2020-07-04 03:30:05 -0500

    Create routes

---
+++ routes/api.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/todos', (req, res, next) => {
+
+});
+
+router.post('/todos', (req, res, next) => {
+
+});
+
+router.delete('/todos/:id', (res, req, next) => {
+
+});
+
+module.exports = router;
