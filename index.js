abd4725b8636c80ac7712e8f750167bd8d470eae
Jude kuti <judekuti@gmail.com>
2020-07-05 18:37:10 -0500

    Paused dev with few bugs

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes/api');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+mongoose.connect(process.env.DB, { useNewUrlParser: true })
+    .then(() => console.log(`Database successfully connected`))
+    .catch(err => console.log(err));
+
+mongoose.Promise = global.Promise;
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -11,8 +21,13 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    res.send('Welcome to my TODO App');
+app.use(bodyParser.json());
+
+app.use('/api', routes);
+
+app.use((err, req, res, next) => {
+    console.log(err)
+    next();
 });
 
 app.listen(port, () => {
